acf5ba87dbb585778a81db1f8d81d89f9fc2f827
oneilk10 <oneilk10@tcd.ie>
2023-11-09 14:32:02 +0000

    fixing init.sh

--- third-year/CSU33031-computer-networks/assignment-2/init.sh
+++ third-year/CSU33031-computer-networks/assignment-2/init.sh
@@ -1,29 +1,50 @@
-# Initialisation script
-# TODO: Make this prettier
-# TODO: Router initialisation and connection
+# initialisation script
 
-# Variables
-subnet=172.20.0.0/16
 
-# Create Network
-echo -e "\n🌐 Creating network 🌐"
-sudo docker network create -d bridge --subnet $subnet a2-network
+# variables
+subnet0=172.20.0.0/16
+subnet1=172.30.0.0/16
+
+# build images
+echo -e "\n👷‍♀️ Building images..."
+echo -e "\n🎮 controller..."
+docker build -t controller ./controller
+echo -e "\n📡 router..."
+docker build -t router ./router
+echo -e "\n🖥️ endpoint..."
+docker build -t endpoint ./endpoint
+
+# create containers
+echo -e "\n📦 Creating containers..."
+echo -e "\n🎮 controller..."
+docker create -ti --name controller --cap-add=ALL controller
+echo -e "\n📡 router..."
+docker create -ti --name router --cap-add=ALL router
+echo -e "\n🖥️ endpoint..."
+docker create -ti --name endpoint --cap-add=ALL endpoint
+
+# create networks
+echo -e "\n🌐 Creating networks..."
+docker network create -d bridge --subnet $subnet0 network0
+docker network create -d bridge --subnet $subnet1 network1
 
 # Copy tools.py to directories
-echo -e "\n🔨 Copying tools.py to directories 🔧"
+echo -e "\n👾 Copying tools library to subdirectories"
+cp tools.py controller/
 cp tools.py endpoint/
 cp tools.py router/
 
-# Create router
-echo -e "\n📡 Creating router 📡"
-sudo docker build -t a2-router ./router
-sudo docker container create --name a2-router --cap-add=ALL a2-router
-sudo docker network connect a2-network a2-router
-
-# Create Endpoints
-echo -e "\n⚜️ Creating network ⚜️"
-sudo docker build -t a2-endpoint ./endpoint
+# connect everything to networks
+echo -e "\n🔌 Connecting things up..."
+docker network connect anetwork0 controller
+docker network connect network1 controller
+docker network disconnect bridge controller
+docker network connect network0 router
+docker network connect network1 router
+docker network disconnect bridge router
 
 # remove tools.py from directories
+echo -e "\n🧹🫧 Cleaning up..."
+rm controller/tools.py
 rm endpoint/tools.py
 rm router/tools.py
